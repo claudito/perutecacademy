8671bda53fa77b5fda7ed3550454ea9e272261f3
Luis Augusto Claudio Ponce <megabyte1507@gmail.com>
2017-07-30 17:15:25 -0500

    tablas clases php01 domingos

---
+++ PHP01/PHP01-TARDE-DOMINGO/clase03/ejemplo01.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Bootstrap 3</title>
+
+<!-- Optimizar el sitio en dispositivos móviles -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Importar CSS de Bootstrap  -->
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+<!-- Importar Jquery -->
+<script src="bootstrap/js/jquery.js"></script>
+
+<!-- Importar Funciones JavaScript de Bootstrap -->
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+
+</head>
+<body>
+
+<div class="container-fluid"> <!-- Inicio Contenedor -->
+
+<div class="row"> <!-- Inicio Fila -->
+
+<div class="col-md-12"> <!-- Inicio Columna -->
+
+<!-- En esta parte debe ir nuestro código -->
+
+
+<div class="alert alert-success">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+  <strong>Mensaje:</strong> Usuario Registrado
+</div>
+
+</div> <!-- Fin Columna -->
+
+</div> <!-- Fin Fila -->
+
+</div> <!-- Fin Contenedor -->
+
+
+</body>
+</html>
+
---
+++ PHP01/PHP01-TARDE-DOMINGO/clase03/ejemplo02.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Css / Tablas</title>
+
+<!-- Optimizar el sitio en dispositivos móviles -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Importar CSS de Bootstrap  -->
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+<!-- Importar Jquery -->
+<script src="bootstrap/js/jquery.js"></script>
+
+<!-- Importar Funciones JavaScript de Bootstrap -->
+<script src="bootstrap/js/bootstrap.min.js"></script>
+
+
+</head>
+<body>
+
+<div class="container-fluid"> <!-- Inicio Contenedor -->
+
+<div class="row"> <!-- Inicio Fila -->
+
+<div class="col-md-12"> <!-- Inicio Columna -->
+
+<!-- Tablas -->
+
+<div class="table-responsive">
+	
+<table class="table table-condensed">
+
+<thead>
+<tr class="info">
+<th>Código</th>
+<th>Nombres</th>
+<th>Apellidos</th>
+<th>Edad</th>
+<th>Fecha de Ingreso</th>
+<th>DNI</th>
+</tr>
+</thead>
+
+<tbody>
+
+<tr>
+<td>1</td>
+<td>Luis Augusto</td>
+<td>Claudio Ponce</td>
+<td>27</td>
+<td>02/07/2016</td>
+<td>46794282</td>
+</tr>
+
+<tr>
+<td>2</td>
+<td>Omar</td>
+<td>Fernandez</td>
+<td>27</td>
+<td>02/07/2016</td>
+<td>01020405</td>
+</tr>
+
+</tbody>
+
+</table>
+
+
+</div>
+
+
+
+
+</div> <!-- Fin Columna -->
+
+</div> <!-- Fin Fila -->
+
+</div> <!-- Fin Contenedor -->
+
+
+</body>
+</html>
+
